512592f555d933e539cfa11d142b4d0bbd72627b
kikiAgustin <kikiagustin62@gmail.com>
2021-01-28 07:22:19 +0600

    mengganti jquery dan velocity dengan versi 3

---
+++ aplikasi/js/script.js
@@ -0,0 +1,28 @@
+function textIntro() {
+    $("#judul-awal span").velocity("transition.slideLeftIn", {
+        stagger: 150,
+        complete: function () {
+            textSubIntro();
+        }
+    });
+}
+
+function textSubIntro() {
+    $("#sub-awal").removeClass("sub-awal");
+    $("#sub-judul-awal").velocity("transition.slideLeftIn", {
+        stagger: 150,
+        complete: function () {
+            btnNilai();
+        }
+    });
+}
+
+function btnNilai() {
+    $("#btn-nilai").removeClass("btn-remove")
+        .velocity("transition.bounceUpIn");
+
+}
+
+$(document).ready(function () {
+    textIntro();
+});
