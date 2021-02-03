4bcaada56c7c930a64c13d3ac449a84d6c0f8b83
kikiAgustin <kikiagustin62@gmail.com>
2021-02-03 07:16:34 +0600

    menambahkan animasi

--- aplikasi/js/script.js
+++ aplikasi/js/script.js
@@ -40,7 +40,7 @@ function animasiIntroOut() {
 // Daftar Dosen
 function navdaftarDosen() {
     $("#navbar").velocity("transition.slideLeftIn", {
-        stagger: 150,
+        stagger: 1050,
         complete: function () {
             daftarDosen();
         }
@@ -48,8 +48,30 @@ function navdaftarDosen() {
 }
 
 function daftarDosen() {
-    // $("#sub-awal").removeClass("sub-awal");
+    $("#daftar_dosen").removeClass("hapus-daftar");
     $("#daftar_dosen .card").velocity("transition.slideLeftIn", {
+        stagger: 150
+    });
+}
+
+// Halaman Home
+
+function halamanHome() {
+    $("#navbar-home").velocity("transition.slideLeftIn", {
+        stagger: 1050,
+        complete: function () {
+            navbarHome();
+        }
+    });
+}
+
+function navbarHome() {
+    $("#home ").removeClass("hapus-home");
+    $("#about ").removeClass("hapus-about");
+    $("#daftar-dosen ").removeClass("hapus-daftar-dosen");
+    $("#contact ").removeClass("hapus-contact");
+    $("#footer ").removeClass("hapus-footer");
+    $("#home .col-sm-12").velocity("transition.slideLeftIn", {
         stagger: 150
     });
 }
@@ -58,4 +80,5 @@ function daftarDosen() {
 $(document).ready(function () {
     textIntro();
     navdaftarDosen();
+    halamanHome();
 });
